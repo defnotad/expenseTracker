7ad638faeab51e3579e27471cfbef28b6b46dea9
defnotad <aditya.arya.ggn@gmail.com>
2020-07-25 20:37:24 +0530

    Functional progress, no editing yet

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
+const lodash = require("lodash");
+// const index = require(__dirname + "/index.js");
 
 const app = express();
 
@@ -8,13 +11,119 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.listen(3000, function () {
-    console.log("Server started on port 3000")
+const editingVendorName = false;
+const editingVendorTag = false;
+
+
+mongoose.connect("mongodb://localhost:27017/expenseTrackerDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const dateSchema = {
+    monthName: String,
+    range: String,
+};
+
+const Date = mongoose.model("Date", dateSchema);
+
+const date1 = new Date({
+    monthName: "June",
+    range: "1 - 7",
+});
+const date2 = new Date({
+    monthName: "June",
+    range: "8 - 14",
+});
+const date3 = new Date({
+    monthName: "June",
+    range: "15 - 21",
+});
+const date4 = new Date({
+    monthName: "June",
+    range: "22 - 28",
+});
+const date5 = new Date({
+    monthName: "June",
+    range: "29 -",
+});
+
+const defaultDates = [date1, date2, date3, date4, date5];
+
+
+const vendorSchema = {
+    name: String,
+    tag: String,
+    expense: {
+        amount: [Number],
+    },
+};
+
+const VendorExpenditure = mongoose.model("VendorExpenditure", vendorSchema);
+
+const vendorExpenditure1 = VendorExpenditure({
+    name: "Vendor 1",
+    tag: "Salary",
+    expense: {
+        amount: [14000, 0, 0, 0, 0],
+    },
+});
+
+const vendorExpenditure2 = VendorExpenditure({
+    name: "Vendor 2",
+    tag: "Raw Materials",
+    expense: {
+        amount: [4878, 0, 0, 0, 0],
+    },
+});
+
+const defaultVendors = [vendorExpenditure1, vendorExpenditure2];
+
+app.get("/", function (req, res) {
+    Date.find({}, function (err, dateResults) {
+        if (dateResults.length == 0) {
+            Date.insertMany(defaultDates, function (err) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log("Added");
+                }
+            });
+        }
+        VendorExpenditure.find({}, function (err, vendorResults) {
+            if (vendorResults.length == 0) {
+                VendorExpenditure.insertMany(defaultVendors, function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log("Vendors added");
+                    }
+                });
+            }
+            res.render("index", { dateArray: dateResults, vendorResults: vendorResults, editingVendorName: editingVendorName, editingVendorTag: editingVendorTag });
+        });
+    });
+});
+
+app.post("/", function (req, res) {
+    // const val = lodash.drop(req.body, 2);
+    console.log(req.body);
+});
+
+app.post("/newVendorEntry", function (req, res) {
+    const vendorExpenditure = new VendorExpenditure({
+        name: "Vendor",
+        tag: "Tag",
+        expense: {
+            amount: [0,0,0,0,0],
+        },
+    });
+    vendorExpenditure.save();
+    res.redirect("/");
+});
+
+app.post("/editVendor", function (req, res) {
+     
 });
 
 
-let dateArray = ["June 1 - 7", "June 8 - 14", "June 15 - 21", "June 22 - 28"];
-
-app.get("/", function (req, res) {
-    res.render("index", { dateArray: dateArray });
+app.listen(3000, function () {
+    console.log("Server started on port 3000")
 });
--- public/index.js
+++ public/index.js
@@ -1,7 +1,18 @@
-let vendorItems = document.querySelectorAll(".dropdown-item");
+// let vendorItems = document.querySelectorAll(".dropdown-item");
 
-for (let i = 0; i < vendorItems.length; i++) {
-    vendorItems[i].addEventListener("click", function () {
-        document.querySelector(".vendor-btn").textContent = this.innerText;
-    });
-}
+// for (let i = 0; i < vendorItems.length; i++) {
+//     vendorItems[i].addEventListener("click", function () {
+//         document.querySelector(".vendor-btn").textContent = this.innerText;
+//     });
+// }
+
+// let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+// let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+// console.log(vw);
+// console.log(vh);
+
+// let size = [vw, vh];
+
+// $(window).resize(function () {
+//     window.resizeTo(size[0], size[1]);
+// });
